1bce2d6e9786c11294bfcc64fe5e9d69ec9e9644
Matt Wilson <wilsonm@ad.loki-hq.com>
2022-02-23 15:14:26 -0500

    Initial Entry

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'zelda';
+  assets : string = "/assets";
+  x : number = 7;
+  y : number = 7;
+  world : string[][] = [];
+  screen : string[][] = [];
+  fullScreen : boolean = false;
+  showGuideLines : boolean = false;
+
+  constructor(private http: HttpClient) {
+    var y = 0;
+    this.http.get(this.assets + '/nes_zelda_overworld_tile_map.txt',
+      {responseType: "text"} ).subscribe(data => {
+      data.split('\n').forEach( (line) => {
+        if( line.length > 0) this.world[y++] = line.split(' ');
+      });
+      this.home();
+    });
+  }
+
+  public setScreen(x : number, y : number) {
+    this.fullScreen = false;
+    if (x<0||x>=16||y<0||y>=8){
+      return;
+    }
+    this.x = x;
+    this.y = y;
+    var screen = [];
+    for(var dy:number = 0; dy<11; dy++){
+      console.log("for loop executed : " + y);
+      screen[dy] = this.world[(y*11)+dy].slice((x*16), (x*16)+16);
+    }
+    this.screen = screen;
+  }
+
+  public home() { this.setScreen(7,7) }
+  public zoomIn() { this.setScreen(this.x, this.y); }
+
+  public zoomOut(){
+    this.fullScreen = true;
+    this.screen = this.world;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports:   [ BrowserModule, FormsModule, HttpClientModule, MatButtonModule, MatButtonToggleModule, MatIconModule, MatSlideToggleModule ],
   providers: [],
   bootstrap: [AppComponent]
 })
